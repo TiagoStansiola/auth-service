979d5e198dd002f8056d74569c079157b8b5e30a
TiagoWalterStansiola <tiago.stansiola@davinci.edu.ar>
2025-05-02 19:36:23 -0300

    commit inicial

---
+++ src/test/java/com/truco/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truco.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
